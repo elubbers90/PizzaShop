3a0e16213240138e933e423f0c18ce0bb7ea3101
Erik Lubbers <e.lubbers90@gmail.com>
2019-06-16 15:55:23 +0200

    Minor styling improvements + clickable title

--- pizza-shop/src/app/app.component.ts
+++ pizza-shop/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pizzaria Il Roma';
+
+  constructor(private router: Router) {
+  }
+
+  toHome() {
+    this.router.navigate(["/"]);
+  }
 }
