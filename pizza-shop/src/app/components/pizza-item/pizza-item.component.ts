da243650077796dfacfbc06c7fd1a7f2117a06dd
Erik Lubbers <e.lubbers90@gmail.com>
2019-06-16 09:47:51 +0200

    Adding pizzas and removing ingredients

--- pizza-shop/src/app/components/pizza-item/pizza-item.component.ts
+++ pizza-shop/src/app/components/pizza-item/pizza-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Pizza } from 'src/app/models/pizza/pizza';
 
 @Component({
@@ -8,6 +8,15 @@ import { Pizza } from 'src/app/models/pizza/pizza';
 })
 export class PizzaItemComponent implements OnInit {
   @Input() pizza: Pizza;
+  @Output() onAdd : EventEmitter<Pizza> = new EventEmitter();
+
+  _selected: boolean;
+  pizzaToAdd: Pizza;
+  @Input()
+  set selected(selected: boolean) {
+    this._selected = selected;
+    this.pizzaToAdd = JSON.parse(JSON.stringify(this.pizza));
+  }
 
   constructor() { }
 
@@ -16,6 +25,21 @@ export class PizzaItemComponent implements OnInit {
 
   getIngredientsString(): String {
     return this.pizza.ingredients.join(", ");
+  }
+
+  toggleIngredient(ingredientToAdd: string) {
+    let index = this.pizzaToAdd.ingredients.findIndex(ingredient => ingredient == ingredientToAdd);
+    if (index != -1) {
+      this.pizzaToAdd.ingredients.splice(index, 1);
+    } else {
+      this.pizzaToAdd.ingredients.push(ingredientToAdd);
+    }
+
+    console.log(this.pizzaToAdd.ingredients.join(", "))
+  }
+
+  addPizza() {
+    this.onAdd.emit(this.pizzaToAdd);
   }
 
 }
--- pizza-shop/src/app/pages/shop-page/shop-page.component.ts
+++ pizza-shop/src/app/pages/shop-page/shop-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaDataService } from 'src/app/services/pizza-data/pizza-data.service';
+import { Pizza } from 'src/app/models/pizza/pizza';
 
 @Component({
   selector: 'app-shop-page',
@@ -8,13 +9,25 @@ import { PizzaDataService } from 'src/app/services/pizza-data/pizza-data.service
   providers: [PizzaDataService],
 })
 export class ShopPageComponent implements OnInit {
+  selectedPizza: Pizza;
 
   constructor(private pizzaService: PizzaDataService) { }
 
   ngOnInit() {
   }
 
+  pizzaIsSelected(pizza: Pizza): boolean {
+    return this.selectedPizza != null && this.selectedPizza.id == pizza.id;
+  }
+
   get pizzas() {
     return this.pizzaService.getPizzas();
+  }
+
+  selectPizzaToAdd(pizza: Pizza) {
+    this.selectedPizza = pizza;
+  }
+
+  addPizza(pizza: Pizza) {
   }
 }
